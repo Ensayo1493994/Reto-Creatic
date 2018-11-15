e8c8aa01d14f8b018d121c8d2d4322d2f1929548
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2018-11-14 20:51:27 -0500

    Login

---
+++ app/src/main/java/com/alejandro/retocreatic/Login.java
@@ -0,0 +1,169 @@
+package com.alejandro.retocreatic;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class Login extends AppCompatActivity  implements GoogleApiClient.OnConnectionFailedListener {
+    private GoogleApiClient googleApiClient;
+    SignInButton btnlogin;
+    public static final int CODE = 777;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    private ProgressBar progressBar;
+    Animation smalltobig, nothingtocome;
+    ImageView logocir;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        progressBar = findViewById(R.id.progresBar);
+        //animacion login
+
+        smalltobig = AnimationUtils.loadAnimation(this,R.anim.smalltobig);
+        logocir = findViewById(R.id.logocir);
+        logocir.startAnimation(smalltobig);
+
+        nothingtocome = AnimationUtils.loadAnimation(this,R.anim.nothingtocome);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,CODE);
+
+                /*
+                if (CaloriasActivity.temp==1){
+                    goMain();
+                }*/
+
+
+            }
+        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    goMainScreen();
+                }
+
+            }
+        };
+
+    }
+    private void goMain() {
+        Intent intent3 = new Intent(Login.this ,MainActivity.class);
+        intent3.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent3);
+        finish();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult (result);
+        }
+    }
+    private void handleSignInResult(GoogleSignInResult result) {
+
+        if (result.isSuccess()){
+            firebaseAuthwhitGoogle(result.getSignInAccount());
+
+        }else{
+            Toast.makeText(this, "No se pudo acceder a la cuenta", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void firebaseAuthwhitGoogle(GoogleSignInAccount signInAccount) {
+        progressBar.setVisibility(View.VISIBLE);
+        btnlogin.setVisibility(View.GONE);
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(signInAccount.getIdToken(), null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+                btnlogin.setVisibility(View.VISIBLE);
+
+                if(!task.isSuccessful()){
+
+                    Toast.makeText(getApplicationContext(), "no se puede autenticar con firebase", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+
+    }
+
+    private void goMainScreen() {
+        Intent intent2 = new Intent(Login.this ,MainActivity.class);
+        intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent2);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if(firebaseAuthListener != null){
+
+            firebaseAuth.removeAuthStateListener(firebaseAuthListener);
+
+        }
+
+
+    }
+}
